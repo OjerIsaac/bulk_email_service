bff7706accb9d0878b5e7a842e02424d09428f93
Ojer Isaac <isaacojerumu@gmail.com>
2023-03-17 16:42:22 +0100

    chores: PHPMailer

--- classes/user.class.php
+++ classes/user.class.php
@@ -12,9 +12,9 @@ session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
 class User
 {
